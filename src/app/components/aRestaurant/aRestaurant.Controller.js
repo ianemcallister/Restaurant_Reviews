10386329ee2ebdc8122fcaf16388fad70c067452
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-05-25 16:43:23 -0700

    feat: Hours Of Opperation Time Formatting
    Storing the values in minutes but displaying in AM/PM.

--- src/app/components/aRestaurant/aRestaurant.Controller.js
+++ src/app/components/aRestaurant/aRestaurant.Controller.js
@@ -27,12 +27,62 @@ class RestaurantController {
     //SERVICE.set(this, restaurantProfileSvc);
     LOGGER.set(this, $log);
     INIT.set(this, () => {
-        
+        //format the times
+        vm.model.hours = vm._formatAllTimes(vm.model.hours);
 
     });
 
     INIT.get(this)();
     
+    LOGGER.get(this).log(vm.model);
+  }
+
+  _formatTime(minutes) {
+    var hours = Math.floor(minutes / 60);
+    var mins = minutes % 60;
+    var A = '';
+
+    if(minutes > 719 && minutes < 1440) {
+      A = " pm";
+    } else {
+      A = " am";
+    }
+
+    if(minutes > 779) {
+      if(minutes < 1500) hours = hours - 12;
+      else hours = hours - 24;
+    }
+
+    var readableTime = hours + ":";
+
+    if(mins < 10) readableTime = readableTime + "0" + mins + A; 
+    else readableTime = readableTime + mins + A; 
+
+    return readableTime;
+  }
+
+  _formatAllTimes(timesObject) {
+    let vm = this;
+    let blockNum = 0;
+
+    //loop through all blocks
+    timesObject.forEach(block => {
+
+      if(typeof block.always == 'undefined') {
+
+        //then through each endpoint
+        Object.keys(block).forEach(key => {
+
+          timesObject[blockNum][key] = vm._formatTime(block[key]);
+        });
+
+      } 
+
+      //incriment the counter
+      blockNum++
+    });
+
+    return timesObject;
   }
 
   submitReview () {
