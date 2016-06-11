d7925efbcdc5a976c3d9de15830784a8b25edbf8
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-06-11 12:56:44 -0700

    fix: Making Sort Order of Restaurants List User Friendly
    Making sure you can tell where the values are coming from.

--- src/app/components/all/AllRestaurants.Controller.js
+++ src/app/components/all/AllRestaurants.Controller.js
@@ -28,26 +28,83 @@ class AllRestaurantsController {
 
     //define local variables
     vm.state = $state.current.name;
-    vm.order = $state.params['sort'];
+    vm.order = {start: $state.params['sort'], new:'', options: {all:{'Restaurant':true, 'Total Reviews':true, 'Star Rating':true, 'Cuisine':true}, available:[]} };
     vm.showFilters = false;
     vm.allRestaurants = restaurants;                        //load the restaurants
     vm.sortProps = FRONTENDDATA.get(this).getSortProps();   //define the sort props
 
     //load the sort order
     vm.sortProps.defineSort(vm.order);
-    //SCOPE.get(this).
 
     //init on page load
     INIT.set(this, () => {
 
-        //sort it as required
-        vm.restaurantList = this.sortBy(vm.allRestaurants, 'restaurant');
+        //sort the list of restaurant as required
+        vm.restaurantList = this.sortBy(vm.allRestaurants, vm.order.start);
+
+        //only use the list of options that you need
+        vm.order.options.available = vm.consolidateSortOptions(vm.order.start, vm.order.options.all);
 
     });
 
     //run initialization
     INIT.get(this)();
 
+    //setup watchers
+    SCOPE.get(vm).$watch(function watchSort() {
+            return vm.order.new;
+        }, function(newVal, oldVal) {
+        
+        //LOGGER.get(vm).log(newVal, oldVal);
+
+        if(typeof newVal !== 'undefined' && newVal !== '') {
+
+            //convert text to title case 
+            var upperCaseValue = vm.toTitleCase(newVal);
+        
+            //LOGGER.get(vm).log(vm.order.options.all, upperCaseValue, vm.order.options.all[upperCaseValue]);
+            //check if the new value is a valid option
+            if(vm.order.options.all[upperCaseValue]) LOGGER.get(vm).log('found', newVal);
+        }
+
+    });
+
+  }
+
+  toTitleCase(newString) {
+    var returnString = '';
+
+    //if it is multiple words, split on spaces
+    var wordsArray = newString.split(' ');
+
+    //LOGGER.get(this).log('number of words:', wordsArray.length);
+
+    //then change the first letter of each word;
+    var wordNumber = 0;
+    wordsArray.forEach(function(word) {
+        wordNumber++;
+        var length = word.length;
+
+        returnString = returnString + word[0].toUpperCase() + word.slice(1,length);
+
+        //if there's another word add the space back in
+        if(wordNumber < wordsArray.length) returnString = returnString + ' ';
+    });
+
+    //LOGGER.get(this).log('returnign this uppercase: ', returnString);
+
+    return returnString;
+  }
+
+  consolidateSortOptions(current, all) {
+    var returnList = []
+
+    Object.keys(all).forEach(function(option) {
+        
+        if(option.toLowerCase() !== current) returnList.push(option);
+    });
+
+    return returnList;
   }
 
   //sort the list based on the user preference
@@ -77,7 +134,7 @@ class AllRestaurantsController {
   viewRestaurant(key) {
     //console.log(key);
     //log the findings
-    //LOGGER.get(this).log(this.restaurantList[key].id);
+    LOGGER.get(this).log(this.restaurantList[key].id);
 
     //redirect to that page
     STATE.get(this).go('restaurant', {id: this.restaurantList[key].id});
